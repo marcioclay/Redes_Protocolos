871350884fd773d4d5af786e7ed949192dbbfb6e
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-10-04 09:48:04 -0300

    Create cliente.py

---
+++ socket/cliente.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST = '127.0.0.1'  # Endereco IP do Servidor (loopback)
+PORT = 5000  # Porta que o Servidor esta usando (identifica qual a aplicacao)
+# Cria o socket do cliente
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST, PORT)  # Forma a tupla de host, porta
+tcp.connect(dest)  # Estabelece a conexao
+
+
+
+while True:
+	msg = input(">> Digite a mensagem: ")
+	
+	msg = msg.encode('UTF-8')  # Codifica a mensagem para UTF-8
+	tcp.send(msg)  # Envio a mensagem para o servidor
+
+
+	msg = tcp.recv(1024)
+	msg = msg.decode('UTF-8')
+	print(dest, msg)
+# ---------------- fim do protocolo --------------
+
+tcp.close()  # fecha a conexao com o servidor
+
