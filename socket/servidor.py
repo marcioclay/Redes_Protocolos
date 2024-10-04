f1d80cdf632b1ef407f8327458f1098e5bd27d10
Márcio Castelo Branco <48799029+marcioclay@users.noreply.github.com>
2024-10-04 09:47:38 -0300

    Create servidor.py

---
+++ socket/servidor.py
@@ -0,0 +1,31 @@
+import socket
+HOST = ''              # Endereco IP do Servidor e o endereco atual do computador
+PORT = 5000            # Porta que o Servidor na maquina
+# Cria o socket do servidor
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT) # Forma a tupla de host, porta
+
+tcp.bind(orig)		# Solicita ao S.O. acesso exclusivo a porta 5000
+tcp.listen(10)		# Entra no modo de escuta
+
+
+print(">>> Aguardando Conexão")
+while True:
+    con, cliente = tcp.accept() # Aceita conexao do cliente
+    print ('Conectado por', cliente)
+    
+    while True:
+#------- inicio do protocolo --------------
+        msg = con.recv(1024)
+        msg = msg.decode('UTF-8')
+        print (cliente, msg)
+
+
+        msg=input(">> Digite a Mensagem: ")
+        msg = msg.encode('UTF-8')
+        con.send (msg)
+
+
+#---------------- fim do protocolo --------------
+
+con.close()		# fecha a conexao com o cliente
